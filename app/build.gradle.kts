aba8b78215de41979f0681e306301af43d513184
Giovani Schiar <giovanischiar@gmail.com>
2023-09-18 10:16:01 -0700

    Update Kotlin and SDK Version, Configure Compose, Add Compose Libraries, Add Main Activity with a Working Hello World Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.schiar.threerule"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "io.schiar.threerule"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,13 +32,30 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
 }
 
 dependencies {
+    implementation("androidx.compose.foundation:foundation-layout-android:1.5.1")
+    val composeVersion = "1.5.1"
+    val wearComposeVersion = "1.2.0"
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("com.google.android.gms:play-services-wearable:18.0.0")
+    // General compose dependencies
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.wear.compose:compose-material:$wearComposeVersion")
+    implementation("androidx.wear.compose:compose-foundation:$wearComposeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
     implementation("androidx.percentlayout:percentlayout:1.0.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
 }
