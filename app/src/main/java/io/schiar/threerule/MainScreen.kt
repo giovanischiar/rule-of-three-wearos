3750be4032857d675427cea89288e3912b45d4c3
Giovani Schiar <giovanischiar@gmail.com>
2024-01-10 15:38:44 -0800

    Remove Unused Imports

--- app/src/main/java/io/schiar/threerule/MainScreen.kt
+++ app/src/main/java/io/schiar/threerule/MainScreen.kt
@@ -1,14 +1,11 @@
 package io.schiar.threerule
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,14 +13,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Text
 import io.schiar.threerule.components.NumberInput
 import java.text.DecimalFormat
-import java.text.NumberFormat
 
 @Composable
 fun MainScreen() {
--- app/src/main/java/io/schiar/threerule/components/NumberInput.kt
+++ app/src/main/java/io/schiar/threerule/components/NumberInput.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.painter.BrushPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
