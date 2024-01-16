9faf3809064547ee9d82cc2216725109a5415bc9
Giovani Schiar <giovanischiar@gmail.com>
2024-01-15 23:09:01 -0800

    Allow Using the Rotary to Scroll the History

--- app/src/main/java/io/schiar/ruleofthree/view/screen/HistoryScreen.kt
+++ app/src/main/java/io/schiar/ruleofthree/view/screen/HistoryScreen.kt
@@ -2,6 +2,8 @@ package io.schiar.ruleofthree.view.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,13 +15,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,6 +33,8 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
 import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.IconButton
 import io.schiar.ruleofthree.R
@@ -35,14 +43,29 @@ import io.schiar.ruleofthree.model.datasource.NumbersDataSource
 import io.schiar.ruleofthree.model.repository.MainRepository
 import io.schiar.ruleofthree.view.components.NumbersHistoryView
 import io.schiar.ruleofthree.viewmodel.HistoryViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalWearFoundationApi::class)
 @Composable
 fun HistoryScreen(viewModel: HistoryViewModel, onBackPressed: () -> Unit = {}) {
     val allPastNumbers by viewModel.allPastNumbers.collectAsState()
 
+    val listState = rememberLazyListState()
+    val focusRequester = rememberActiveFocusRequester()
+    val coroutineScope = rememberCoroutineScope()
+
     Box(modifier = Modifier.background(color = colorResource(id = R.color.backgroundColor))) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            LazyColumn {
+            LazyColumn(
+                modifier = Modifier
+                    .onRotaryScrollEvent {
+                        coroutineScope.launch { listState.scrollBy(it.verticalScrollPixels) }
+                        true
+                    }
+                    .focusRequester(focusRequester)
+                    .focusable(),
+                state = listState
+            ) {
                 item { Spacer(modifier = Modifier.fillMaxWidth().height(25.dp)) }
                 items(count = allPastNumbers.size) {
                     val (a, b, c, result) = allPastNumbers[it]
