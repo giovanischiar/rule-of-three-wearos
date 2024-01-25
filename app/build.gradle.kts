2d9d90077ae1cdccb13379fe6140dd1cd39a80b9
Giovani Schiar <giovanischiar@gmail.com>
2024-01-24 19:23:12 -0800

    Add Instrumented Tests to Test the Database, Copy Data Source Unit Tests to Android Test Folder, Organize Dependencies, Remove Some Unnecessary Libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -44,34 +44,42 @@ android {
 dependencies {
     implementation("androidx.compose.foundation:foundation-layout-android:1.5.4")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.wear.compose:compose-material3:1.0.0-alpha15")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    testImplementation("org.testng:testng:6.9.6")
-    val composeVersion = "1.5.4"
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
-    val wearComposeVersion = "1.2.1"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     // General compose dependencies
+    val composeVersion = "1.5.4"
+    val wearComposeVersion = "1.2.1"
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.wear.compose:compose-material:$wearComposeVersion")
     implementation("androidx.wear.compose:compose-foundation:$wearComposeVersion")
+
+    // Preview
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.wear:wear-tooling-preview:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.wear.compose:compose-material3:1.0.0-alpha15")
+
+    // Material
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.wear.compose:compose-material:$wearComposeVersion")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.google.android.gms:play-services-wearable:18.1.0")
     implementation("androidx.percentlayout:percentlayout:1.0.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Room for database persistence
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
+
+    //Test
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 task("generateIcons", type = Exec::class) {
---
+++ app/src/androidTest/java/io/schiar/ruleofthree/model/datasource/CurrentCrossMultipliersDataSourceTest.kt
@@ -0,0 +1,74 @@
+package io.schiar.ruleofthree.model.datasource
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import io.schiar.ruleofthree.RuleOfThreeDatabase
+import io.schiar.ruleofthree.model.CrossMultiplier
+import io.schiar.ruleofthree.model.datasource.util.toCurrentCrossMultiplierEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+class CurrentCrossMultipliersDataSourceTest {
+    private lateinit var database: RuleOfThreeDatabase
+    private lateinit var currentCrossMultiplierDataSource: CurrentCrossMultiplierDataSource
+    private val crossMultiplier = CrossMultiplier(valueAt00 = 3, valueAt01 = 4.3)
+
+    @Before
+    fun createDatabase() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        database = Room.inMemoryDatabaseBuilder(
+            context = context,
+            klass = RuleOfThreeDatabase::class.java
+        ).build()
+        currentCrossMultiplierDataSource = CurrentCrossMultiplierDataSource(
+            currentCrossMultiplierDAO = database.currentCrossMultiplierDAO().apply {
+                insertWithTimestamp(
+                    currentCrossMultiplierEntity = crossMultiplier.toCurrentCrossMultiplierEntity()
+                )
+            }
+        )
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDatabase() {
+        database.close()
+    }
+
+    @Test
+    fun `Retrieve Current Cross Multiplier`() = runBlocking {
+        // Given
+        val expectedCurrentCrossMultiplier = crossMultiplier
+
+        //When
+        val actualCurrentCrossMultiplier = currentCrossMultiplierDataSource
+            .retrieveCurrentCrossMultiplier()
+
+        // Then
+        assertEquals(expectedCurrentCrossMultiplier, actualCurrentCrossMultiplier)
+    }
+
+    @Test
+    fun `Update Current Cross Multiplier`() = runBlocking {
+        // Given
+        val expectedCurrentCrossMultiplier = CrossMultiplier(
+            valueAt00 = 3,
+            valueAt01 = 4.3,
+            valueAt10 = 3.4
+        )
+
+        //When
+        currentCrossMultiplierDataSource
+            .updateCurrentCrossMultiplier(crossMultiplierUpdated = expectedCurrentCrossMultiplier)
+
+        // Then
+        val actualCurrentCrossMultiplier = currentCrossMultiplierDataSource
+            .retrieveCurrentCrossMultiplier()
+        assertEquals(expectedCurrentCrossMultiplier, actualCurrentCrossMultiplier)
+    }
+}
